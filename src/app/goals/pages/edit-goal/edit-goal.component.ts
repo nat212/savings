f491a9f7c50ae93a76d450035814f69cc701be66
Natasha Draper <natasha@draper.net.za>
2020-10-03 21:58:54 +0200

    dev

--- src/app/goals/pages/edit-goal/edit-goal.component.ts
+++ src/app/goals/pages/edit-goal/edit-goal.component.ts
@@ -1,6 +1,8 @@
+import { getCurrencySymbol } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { SettingsService } from 'src/app/settings/services/settings.service';
 import { fabShowHide } from 'src/app/shared/animations/fab';
 
 @Component({
@@ -13,7 +15,11 @@ export class EditGoalComponent implements OnInit {
   public goalForm: FormGroup;
   public newGoal = false;
 
-  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private settings: SettingsService,
+  ) {}
 
   public ngOnInit(): void {
     this.goalForm = this.formBuilder.group({
@@ -21,5 +27,13 @@ export class EditGoalComponent implements OnInit {
       amount: [null, Validators.required],
     });
     this.newGoal = !this.route.snapshot.paramMap.has('goal_id');
+  }
+
+  get currencyCode(): string {
+    return this.settings.getCurrency().code;
+  }
+
+  get currencySymbol(): string {
+    return getCurrencySymbol(this.currencyCode, 'narrow');
   }
 }
